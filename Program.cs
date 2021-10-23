51951fe5b7b733d48c16b786f58260f22ad80b95
Tak Yu Chan <frankychantakyu@yahoo.com.hk>
2021-10-23 13:46:42 +1300

    init

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTimeOffset now = DateTimeOffset.Now;
+            DateTimeOffset utcNow = DateTimeOffset.UtcNow;
+
+            DateTime dtNow = DateTime.Now;
+            DateTime dtNowInUTCWithoutTimeZoneInfo = TimeZoneInfo.ConvertTimeToUtc(dtNow);
+
+            TimeZoneInfo localSystemTimeZone = TimeZoneInfo.Local;
+            
+            
+            Console.WriteLine($"Current time (local) in UTC  {now}");
+            Console.WriteLine($"Current UTC time {utcNow}");
+
+            Console.WriteLine($"Date time now: {dtNow}");
+            Console.WriteLine($"Date time to UTC now: {dtNowInUTCWithoutTimeZoneInfo}");
+
+            Console.WriteLine($"Local system time zone {localSystemTimeZone}");
+            Console.WriteLine($"UTC to dateTime now: {TimeZoneInfo.ConvertTimeFromUtc(dtNowInUTCWithoutTimeZoneInfo, localSystemTimeZone)}");
+        }
+    }
+}
